4aee0abeb4de53c50a708089b3335d94ddb98ef4
Dori Minante <dorim.dev@gmail.com>
2025-06-13 20:19:34 -0300

    dia 2

---
+++ calculadora_simples.py
@@ -0,0 +1,8 @@
+# ● Peça ao usuário para inserir dois números e armazene-os em variáveis.
+# ● Calcule a soma, subtração, multiplicação e divisão desses números.
+# ● Exiba os resultados.
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = número 1 
---
+++ ex1.py
@@ -0,0 +1,29 @@
+# Crie um programa que declare as seguintes variáveis: 
+# ● nome: seu nome.
+# ● idade: sua idade.
+# ● altura: sua altura.
+# ● estudante: se você é estudante ou não (True/False).
+
+nome = "Dori"
+idade = 31
+altura = 1.60
+estudante = True
+
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("Estudante: ", estudante)
+
+# Ano do nascimento
+ano_nascimento = 2025 - idade
+print("Ano do nascimento: ", ano_nascimento)
+
+# Verificar se é maior de idade
+maior_de_idade = idade >= 18
+print("Maior de idade: ", maior_de_idade)
+
+# Manipulação de Strings
+# Crie uma variável frase que contenha a mensagem: "Olá, meu nome é [seu nome] e eu tenho [sua idade] anos."
+
+frase = ("Olá, meu nome é " + nome + " e eu tenho " + str(idade) + " anos.")
+print(frase)
