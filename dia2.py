670ac682a0b30fd73cd14058fdd8f70c54cdeda6
Dori Minante <dorim.dev@gmail.com>
2025-06-13 20:05:57 -0300

    dia 2

---
+++ dia2.py
@@ -0,0 +1,38 @@
+# Variáveis
+idade = 25
+nome = "Dori"
+altura = 1.68
+
+#Tipos de dados
+# int, float, textos = strings, bool = true ou false
+
+cidade = 'São Paulo'
+esta_logado = True
+
+print(nome)
+nome = 2
+print(nome)
+
+# Verificar o tipo de dado
+print(type(nome)) # int = Integer
+print(type(cidade)) # str = String
+print(type(esta_logado)) # bool = boolean
+print(type(altura)) # float
+
+# Operadores
+print(5 + 1.2)
+print(10 * 2)
+print(5 / 10)
+print(50 - 25)
+
+# Concatenação = unir com o simbolo +
+nomeUsuario = "João"
+
+frase = "Olá " + nomeUsuario + ", tudo bem?"
+print(frase)
+
+# Comparações
+maior = 15 > 2
+print(maior)
+
+
